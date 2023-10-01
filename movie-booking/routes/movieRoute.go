d09ed06a61382761f505612d401be73a2c2af3df
SAF2k <safwathkhan2@gmail.com>
2023-10-01 19:05:23 +0530

    changes in Movie

--- movie-booking/routes/movieRoute.go
+++ movie-booking/routes/movieRoute.go
@@ -9,8 +9,8 @@ import (
 func MovieRoute(r *mux.Router) {
 	// Movie routes
 	r.HandleFunc("/movies", controllers.GetMovies).Methods("GET")
-	r.HandleFunc("/movies/{id:[0-9]+}", controllers.GetMovie).Methods("GET")
+	r.HandleFunc("/movie/{id}", controllers.GetMovie).Methods("GET")
 	r.HandleFunc("/movies", controllers.CreateMovie).Methods("POST")
-	r.HandleFunc("/movies/{id:[0-9]+}", controllers.UpdateMovie).Methods("PUT")
-	r.HandleFunc("/movies/{id:[0-9]+}", controllers.DeleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", controllers.UpdateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", controllers.DeleteMovie).Methods("DELETE")
 }
